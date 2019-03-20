c27820d24a7ed5a2d8f2d17db532ab0280c4934e
Segundo Bartra <segundobartra@gmail.com>
2019-03-20 22:48:13 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,11 +1,10 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
-def display_board(
-board = [" ", " ", " ", " ", " ", " ", " ", " ", " "])
-  puts 'display board'
-#board output
-puts "  |  |  "
-puts "-----------"
-puts "  |  | "
-puts "-----------"
-puts "  |  |  "
-end
+def display_board(squares)
+
+system 'clear'
+4   puts "#{squares[1]}|#{squares[2]}|#{squares[3]}"
+5   puts "-----"
+6   puts "#{squares[4]}|#{squares[5]}|#{squares[6]}"
+7   puts "-----"
+8   puts "#{squares[7]}|#{squares[8]}|#{squares[9]}"
+9 end
