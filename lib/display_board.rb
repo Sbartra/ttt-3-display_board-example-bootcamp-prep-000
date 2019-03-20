67f6f3d736603ad9b936409c61411f22d6ddb1b1
Segundo Bartra <segundobartra@gmail.com>
2019-03-20 22:52:35 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,10 +1,38 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
-def display_board(squares)
 
-system 'clear'
-4   puts "| |"
-5   puts "-----"
-6   puts "| | "
-7   puts "-----"
-8   puts "| | "
-9 end
+$moveArr = []
+$colDivider = "|"
+$rowDivider = "-----------"
+
+def dispLocation(move)
+  print $moveArr[move]
+end
+
+def display_board
+  numOfRows = 2
+  numOfCols = 2
+  for i in 0..numOfRows do
+    for j in 0..numOfCols do
+      dispLocation(i*3 + j)
+      if j < numOfRows
+        print $colDivider
+      else
+        puts ""
+      end
+    end
+    if i < numOfCols
+      puts $rowDivider
+    end
+  end
+end
+
+def initializeVal
+  initVal = "   "
+  for i in 0..8 do
+    $moveArr << initVal
+  end
+  return $moveArr
+end
+
+initializeVal
+display_board
